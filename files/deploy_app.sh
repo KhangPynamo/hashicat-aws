13a561c9c0d233925dfda8d69fc0ecf5a8aeaceb
Khang Nguyen <141895443+KhangPynamo@users.noreply.github.com>
2023-08-24 11:23:33 +0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. The only trophy you deserve is catastrophy.
+  Welcome to ${PREFIX}'s app. The only trophy you deserve is catastrophy since you made me paralyzed.
   <!-- END -->
 
   </div>
